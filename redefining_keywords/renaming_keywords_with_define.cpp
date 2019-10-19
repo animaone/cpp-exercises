391279ee7f9fe10dd7f45c1a75c07fce5f7e19f5
animaone <31348553+animaone@users.noreply.github.com>
2019-10-19 14:17:56 -0300

    Update renaming_keywords_with_define.cpp

--- redefining_keywords/renaming_keywords_with_define.cpp
+++ redefining_keywords/renaming_keywords_with_define.cpp
@@ -1,17 +1,5 @@
-#include <iostream>
-using namespace std;
-#define false true
-#define true !false
-#define somethingStrangeIsHappening if
-#define automaaaatic auto
-#define tell cout
-#define by return
-#define begin main
-#define hello {
-#define bye }
-#define boo <<
-#define THE_BIG_SEPARATOR ,
-#define THE_END_OF_LINE ;
+#include "redefine.h"
+
 int begin(void)
 hello
 	#define int float
