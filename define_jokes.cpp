e9477f123266ae7bdffb4e79ae60b621f0c40b9d
animaone <31348553+animaone@users.noreply.github.com>
2019-10-19 14:09:57 -0300

    Update define_jokes.cpp

--- define_jokes.cpp
+++ define_jokes.cpp
@@ -8,20 +8,22 @@ using namespace std;
 #define by return
 #define begin main
 #define hello {
-#define tchau }
+#define bye }
 #define boo <<
+#define THE_BIG_SEPARATOR ,
+#define THE_END_OF_LINE ;
 int begin(void)
 hello
 	#define int float
 	somethingStrangeIsHappening (false)
 	hello
-		automaaaatic listee = hello"Some", "some", "items"};
+		automaaaatic listee = hello "Some" THE_BIG_SEPARATOR "some" THE_BIG_SEPARATOR "items" bye THE_END_OF_LINE
 		for (automaaaatic item  : listee)
 		hello
-			tell boo item boo "\n";
-		tchau		
+			tell boo item boo "\n" THE_END_OF_LINE
+		bye		
 		int k = 2.3949394929349952935;
-		tell boo "int32 have no decimal cases:" boo k;
-	tchau
+		tell boo "int32 have no decimal cases:" boo k THE_END_OF_LINE
+	bye
 	by 0;
-tchau
+bye
