b0ac0a081c8622a915081d7557359bb8f6835ccf
animaone <31348553+animaone@users.noreply.github.com>
2019-10-19 14:18:23 -0300

    Create redefine.h

---
+++ redefining_keywords/redefine.h
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#define false true
+#define true !false
+#define somethingStrangeIsHappening if
+#define automaaaatic auto
+#define tell cout
+#define by return
+#define begin main
+#define hello {
+#define bye }
+#define boo <<
+#define THE_BIG_SEPARATOR ,
+#define THE_END_OF_LINE ;
