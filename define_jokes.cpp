571d0a992322d864a8fadad6f02d8688302bc7c0
animaone <31348553+animaone@users.noreply.github.com>
2019-10-19 14:02:26 -0300

    Create define_jokes.cpp

---
+++ define_jokes.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+#define false true
+#define true !false
+#define somethingStrangeIsHappening if
+#define automaaaatic auto
+#define tell cout
+#define by return
+#define begin main
+#define hello {
+#define tchau }
+#define boo <<
+int begin(void)
+hello
+	#define int float
+	somethingStrangeIsHappening (false)
+	hello
+		automaaaatic listee = hello"Some", "some", "items"};
+		for (automaaaatic item  : listee)
+		hello
+			tell boo item boo "\n";
+		tchau		
+		int k = 2.3949394929349952935;
+		tell boo "int32 have no decimal cases:" boo k;
+	tchau
+	by 0;
+tchau
